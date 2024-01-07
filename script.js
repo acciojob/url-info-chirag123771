a7d274a57ffa553ce470686d8da1d3f552052708
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-07 23:33:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL using the location object
+const currentURL = window.location.href;
+
+// Calculate the length of the URL
+const urlLength = currentURL.length;
+
+alert("The length of the URL is: " + urlLength);
+
